8941b3493f2af4cb5b3a6a69ec4158c0b750d805
Mohamed Ahmed Mokhtar <mohamedAhmedMokhtarElkomy>
2023-01-05 19:10:48 +0200

    global Module and files structure for data and models

--- RNN/collectData.py
+++ RNN/collectData.py
@@ -1,35 +1,6 @@
-from globalClass import *
-import mediapipe as mp
-import cv2
-
-
-mpHolistics = mp.solutions.holistic
-mpDrawing = mp.solutions.drawing_utils
-
-def mediapipeDetection(img, model):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img.flags.writeable = False
-    results = model.process(img)
-    img.flags.writeable = True
-    return results
-
-def getKeyPoints(result):
-    face = np.array([[res.x, res.y, res.z] for res in result.face_landmarks.landmark]).flatten() if result.face_landmarks else np.zeros(1404)
-    leftHandLandMarks = np.array([[res.x, res.y, res.z] for res in result.left_hand_landmarks.landmark]).flatten() if result.left_hand_landmarks else np.zeros(21*3)
-    rightHandLandMarks = np.array([[res.x, res.y, res.z] for res in result.right_hand_landmarks.landmark]).flatten() if result.right_hand_landmarks else np.zeros(21*3)
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in result.pose_landmarks.landmark]).flatten()if result.pose_landmarks else np.zeros(132)
-    return np.concatenate([face, leftHandLandMarks, rightHandLandMarks, pose])
-
-def drawLandmarks(img, results):
-    mpDrawing.draw_landmarks(img, results.face_landmarks, mpHolistics.FACEMESH_TESSELATION)
-    mpDrawing.draw_landmarks(img, results.pose_landmarks, mpHolistics.POSE_CONNECTIONS)
-    mpDrawing.draw_landmarks(img, results.left_hand_landmarks, mpHolistics.HAND_CONNECTIONS)
-    mpDrawing.draw_landmarks(img, results.right_hand_landmarks, mpHolistics.HAND_CONNECTIONS)
-    
+from globalClass.globalClass import * 
 
 if __name__ == "__main__":
-
-
 
     for action in actions:
         for sequence in range(noSequences):
--- RNN/globalClass.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import os
-
-
-dataPath = os.path.join('../GesturesData')
-actions = np.array(['right click', 'left click'])
-noSequences = 30
-sequenceLen = 30
