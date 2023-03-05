bfd0f6424676eeb06d8c399247d28de6e437b250
NourhanElyamany <68388481+NourhanElyamany@users.noreply.github.com>
2023-03-05 10:46:56 +0200

    "nothingg" data

--- Gestures/globalModule/globalModule.py
+++ Gestures/globalModule/globalModule.py
@@ -14,7 +14,8 @@ gesturesDataPath = os.path.join(f'data/gestures')
 modelPath = os.path.join(f'Models')
 log_dir = os.path.join('Logs')
 
-actions = np.array(['click', 'swipe up', 'volume up', 'close', 'pause&play','nothing'])
+actions = np.array(['click', 'swipe up', 'volume up', 'close', 'pause&play', 'nothingg'])
+print(actions)
 grides = np.array(['leftUP, leftDown, rightUp, rightDown'])
 noSequences = 50
 sequenceLen = 30
