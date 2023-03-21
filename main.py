852aa63d7389594def4f0cbcebdd8723d7058555
karimkohel <kareemkohel@gmail.com>
2023-03-22 00:14:44 +0200

    started mock main file

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+The main file for controlling the entire project
+
+Plan:
+    - load all supporting modules
+    - load the gesture & iris models
+    - start capturing and reacting to results
+
+"""
