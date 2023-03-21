f11501cd0fed957eb85ed9a96593ea5e930ae2f2
karimkohel <kareemkohel@gmail.com>
2023-03-22 00:35:28 +0200

    refined main file plan

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 """
 The main file for controlling the entire project
 
-Plan:
-    - load all supporting modules
-    - load the gesture & iris models
-    - start capturing and reacting to results
+Pseudo code
+------------
+
+Setup LOOP:
+- calibrate
+- if calibrated move on to normal loop
+
+Normal LOOP:
+- load models
+- START LOOP
+    - capture frames 
+    - create thread for iris, returns grid location
+    - create thread for gesture, returns gesture
+    - wait for both threads to join
+    - decision making with both inputs
+    - run appropriate decision with actuator
 
 """
