be7632e4a00d21581fb802671e246605051864cc
NourhanElyamany <68388481+NourhanElyamany@users.noreply.github.com>
2023-04-16 15:47:57 +0200

    os actions

---
+++ actions.py
@@ -0,0 +1,36 @@
+import osascript
+import sys
+
+
+def runAction(gesture):
+    
+    if gesture == 'volume up':
+        
+    
+        # get volume settings
+
+        result = osascript.osascript('get volume settings')
+        volInfo = result[1].split(',')
+        outputVol = volInfo[0].replace('output volume:', '')
+        print(outputVol)
+
+        # set volume
+
+        target_volume = 40
+        osascript.osascript("set volume output volume {}".format(target_volume))
+        
+    elif gesture == 'click':
+        
+        tell application "System Events"
+            click at {123,456}
+        end tell
+
+
+
+
+        
+
+
+
+
+
