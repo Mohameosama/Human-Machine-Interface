ae8aa9d42658803bd12393a23a522acd954c2240
mohamedAhmedMokhtarElkomy <mohamed.mokhtar.elkomy@gmail.com>
2023-07-08 03:56:34 +0300

    gesture works well in realtime

--- Gestures/main.py
+++ Gestures/main.py
@@ -26,7 +26,8 @@ import cv2
 import gestures as Gesture
 import pyautogui
 from cornea.classes.gazeTracker import GazeTracker
-
+import mediapipe as mp
+import numpy as np
 
 def predictGesture(framesQueue, resultsQueue):    
     # frames = []
@@ -46,7 +47,37 @@ def predictIris(framesQueue, resultsQueue):
             coordinates = gazeTracker.track_gaze(frames)
             pyautogui.moveTo(coordinates[0], coordinates[1])
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(2)
+
+
+
+
+
+
+
+
+
+
+
+mpHolistics = mp.solutions.holistic
+def isHandFound(frame):    
+    
+    with mpHolistics.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        results = holistic.process(frame)
+        if results.right_hand_landmarks == None:
+            return False
+        else:
+            return True
+        
+
+
+
+
+
+
+
+
+
 
 numberOfFrames = 30 #TODO should get numberOfFrames from gestures module
 
@@ -67,15 +98,42 @@ frames_list = []
 
 cv2.waitKey(3000)  # wait 3 seconds to load the whole project and threads
 
-while(1):
-    success ,frame = camera.read()    
-    frames_list.append(frame)    
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+handFramesCount = 0
 
-    if len(frames_list) >= 30 and framesQueue_gestures.qsize() <= 0:
-        frames_list = frames_list[-30:]
-        # framesQueue_iris.put(frames_list)
+
+while(1):
+    success ,frame = camera.read()   
+    cv2.imshow("camera", frame) 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    frames_list.append(frame)
+
+    # Check if hand is found on frame or not
+    imageRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(imageRGB)
+    if results.multi_hand_landmarks:
+        handFramesCount += 1
+    else:
+        handFramesCount = 0
+
+    if handFramesCount == 30 and framesQueue_gestures.qsize() <= 0:        
+        
+        frames_list = frames_list[-30:]        
         framesQueue_gestures.put(frames_list)
+
+       
+
+    # if len(frames_list) >= 30 and framesQueue_gestures.qsize() <= 0:
+    #     frames_list = frames_list[-30:]
+    #     # framesQueue_iris.put(frames_list)
+    #     framesQueue_gestures.put(frames_list)
+
+
     if len(frames_list) >= 3 and framesQueue_iris.qsize() <= 0:
         temp_frames = frames_list[-3:]
         framesQueue_iris.put(temp_frames)
-        
+
+cv2.destroyAllWindows()
